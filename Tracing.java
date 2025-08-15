b9995dcd60671acc1f122720400eef6a9f2ec9bb
asgharkrk <45304174+asgharkrk@users.noreply.github.com>
2025-08-15 15:36:15 -0400

    Update Tracing.java

--- Tracing.java
+++ Tracing.java
@@ -24,6 +24,6 @@
         textLocator.click();
     }
 
-//span[text()='ABC Supply Co. Inc' and @class='font-italic text-grey font-weight-bold small']/ancestor::td[1]//a[@id='TASK_COLSOLE_FORM:varcustomerRequestsid:3:review_credit_app_cmd']
-//span[text()='ABC Supply Co. Inc' and @class='font-italic text-grey font-weight-bold small']/ancestor::td[1]//a[contains(@onclick, 'PrimeFaces.ab') and contains(., 'REQPJ')]
-
+page.getByLabel("Unread L&W Support, LW Supply [EXTERNAL] LW Supply: Verify your Email")
+                .getByText("L&W Support", new Page.GetByTextOptions().setExact(false))
+                .click();
