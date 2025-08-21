1c0e1756151a6e6d9a09d12415f38c74b56f96c3
asgharkrk <45304174+asgharkrk@users.noreply.github.com>
2025-08-21 14:30:06 -0400

    Update Tracing.java

--- Tracing.java
+++ Tracing.java
@@ -21,3 +21,18 @@ Locator stateLabel = section.locator("label").filter(new Locator.FilterOptions()
 
 // Now, inside that same section, locate the label with text "select state/province-" and click it
 section.locator("label").filter(new Locator.FilterOptions().setHasText("select state/province-")).click();
+
+===============================================================================================================
+
+// Find the label, go to its sibling combobox, then click the trigger
+Locator stateLabel = page.getByText("State/Province", new Page.GetByTextOptions().setExact(true));
+Locator combobox = stateLabel.locator("xpath=preceding-sibling::div[contains(@class, 'ui-selectonemenu')]");
+combobox.locator(".ui-selectonemenu-trigger").click();
+
+
+// Similar to your original, but target the trigger for opening
+Locator section = page.locator("span.ui-float-label.ui-float-label2");
+Locator stateLabel = section.getByText("State/Province", new Locator.GetByTextOptions().setExact(true));
+Locator combobox = stateLabel.locator("xpath=preceding-sibling::div[contains(@class, 'ui-selectonemenu')]");
+combobox.locator(".ui-selectonemenu-trigger").click();
+
